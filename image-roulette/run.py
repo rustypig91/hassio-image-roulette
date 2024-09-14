002e4c0d66ff8fc234352288a09afae01ddc9e82
Christoffer Zakrisson <rustypig91@gmail.com>
2024-09-14 19:59:17 +0200

    Add support for custom overlay

--- image-roulette/run.py
+++ image-roulette/run.py
@@ -1,23 +1,58 @@
 import os
+import sys
 import random
-from flask import Flask, send_file, render_template
+from flask import Flask, send_file, render_template, make_response
 
-app = Flask(__name__)
+templates_folder = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "templates")
 
 IMAGES_PATH = os.getenv("IMAGES_PATH")
+if (IMAGES_PATH is None) or (not os.path.isdir(IMAGES_PATH)):
+    IMAGES_PATH = "example"
+
+PERIOD_SECONDS = os.getenv("PERIOD_SECONDS")
+if (PERIOD_SECONDS is None):
+    PERIOD_SECONDS = 600
+
+
+custom_script = os.path.join(IMAGES_PATH, "custom.py")
+if (os.path.exists(custom_script)):
+    sys.path.append(IMAGES_PATH)
+    from custom import custom_body  # type: ignore
+    custom_html = os.path.join(IMAGES_PATH, "custom.html")
+    if os.path.exists(custom_html):
+        if os.path.islink(os.path.join(templates_folder, "custom.html")):
+            os.remove(os.path.join(templates_folder, "custom.html"))
+        os.symlink(os.path.abspath(custom_html),
+                   os.path.join(templates_folder, "custom.html"))
+else:
+    def custom_body():
+        return ""
+
+app = Flask(__name__, static_folder=IMAGES_PATH,
+            template_folder=templates_folder)
+
 
 @app.route('/random')
 def serve_random_picture():
+    # if IMAGES_PATH is None:
+    #     response = make_response("Images path not setup", 200)
+    #     response.mimetype = "text/plain"
+    #     return response
 
     # Get a list of all the picture files in the folder
-    picture_files = [file for file in os.listdir(IMAGES_PATH) if file.lower().endswith(('.jpg', '.jpeg', '.png'))]
+    picture_files = [file for file in os.listdir(
+        IMAGES_PATH) if file.lower().endswith(('.jpg', '.jpeg', '.png'))]
+    # raise Exception(picture_files)
     if len(picture_files) == 0:
         return f"No images found in image path ({IMAGES_PATH})"
     # Choose a random picture from the list
     random_picture = random.choice(picture_files)
 
-    # Return the picture file
-    return send_file(os.path.join(IMAGES_PATH, random_picture))
+    return render_template('random.html', random_picture=random_picture, pictures=picture_files, period_seconds=PERIOD_SECONDS, custom_body=custom_body())
+    # # Return the picture file
+    # return send_file(os.path.join(IMAGES_PATH, random_picture))
+
 
 @app.route('/')
 def index():
@@ -30,5 +65,4 @@ if __name__ == '__main__':
     logger = logging.getLogger("waitress")
     logger.setLevel(logging.INFO)
     logger.error(f"Sending images from: {IMAGES_PATH}")
-
     serve(app, host='0.0.0.0', port=5000)
--- image-roulette/run.sh
+++ image-roulette/run.sh
@@ -2,6 +2,9 @@
 
 IMAGES_PATH=$(bashio::config 'images_directory')
 export IMAGES_PATH
+PERIOD_SECONDS=$(bashio::config 'period_seconds')
+export PERIOD_SECONDS
 
+. /image-roulette/venv/bin/activate
 cd /image-roulette
 python3 run.py
